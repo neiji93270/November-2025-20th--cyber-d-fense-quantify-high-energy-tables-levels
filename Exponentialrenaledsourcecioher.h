94fdb004213cdf82e6b71d74f4f2279caec3304c
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-11-20 17:35:24 +0100

    Create Exponentialrenaledsourcecioher.h

---
+++ Exponentialrenaledsourcecioher.h
@@ -0,0 +1,6 @@
+//15 Exponential formula, implicit, 16 Extended generator
+//generators would be key concept on ui/ux models for async cents 
+//so everything is enough complicated to use pro pngroups. morphisms would fail
+//because the range of valies of classes are clamped to better understanding on one time pad systems.
+//then, this program lattice would fail if we use mptivee 
+//see :
