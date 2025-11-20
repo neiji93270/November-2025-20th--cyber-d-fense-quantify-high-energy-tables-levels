838ef76b57066bb919aaf6a024f15315235de137
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-11-20 17:36:07 +0100

    Update Exponentialrenaledsourcecioher.h

--- Exponentialrenaledsourcecioher.h
+++ Exponentialrenaledsourcecioher.h
@@ -3,4 +3,4 @@
 //so everything is enough complicated to use pro pngroups. morphisms would fail
 //because the range of valies of classes are clamped to better understanding on one time pad systems.
 //then, this program lattice would fail if we use mptivee 
-//see :
+//see :https://www.google.com/search?q=john+nash+first+regular+markiv+cipher&client=ms-android-xiaomi-terr1-rso2&sca_esv=88a84ed169363447&biw=360&bih=697&udm=36&sxsrf=AE3TifN0zsvSGPbS5ddN2lbNUx5-zUdHeQ%3A1763656211922&ei=E0IfabWIOM7_nsEP6r23yAo&oq=john+nash+first+regular+markiv+cipher&gs_lp=EhNtb2JpbGUtZ3dzLW1vZGVsZXNzIiVqb2huIG5hc2ggZmlyc3QgcmVndWxhciBtYXJraXYgY2lwaGVyMgcQIRigARgKMgcQIRigARgKSMeSAVCeC1jlkAFwCXgAkAECmAGdBaAB0S2qAQsxMi4zMy4xLjUtMbgBA8gBAPgBAZgCL6ACySPCAggQABiwAxjvBcICCxAAGIAEGLADGKIEwgIFECEYoAHCAgUQABjvBcICBRAhGJ8FwgIGEAAYFhgewgIEECEYFcICCBAAGIAEGMsBwgIFEAAYgATCAgkQABiABBgTGA3CAggQABgTGA0YHsICCBAAGBMYFhgemAMAiAYBkAYDkgcHMTMuMzMuMaAHk7kBsgcGNC4zMy4xuAfzIsIHCTItMTguMjguMcgHlQM&sclient=mobile-gws-modeless
